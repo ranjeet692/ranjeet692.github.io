497190c7386ec028e7f369a6d468d494c296c965
Ranjeet Kumar <ranjeetkumar692@gmail.com>
2023-03-15 09:21:04 +0530

    post data type to jsonp

--- dist/js/sdid_bundle-b1d5b2646b.js
+++ dist/js/sdid_bundle-b1d5b2646b.js
@@ -6573,8 +6573,7 @@
 				function startSend(postData) {
 					$.ajax({
 						url: "https://" + ip_address + "/gpu_fp",
-						headers: {referrerPolicy: 'no-referrer-when-downgrade'},
-						dataType: "json",
+						dataType: "jsonp",
 						contentType: 'application/json',
 						type: 'POST',
 						data: JSON.stringify(postData),
